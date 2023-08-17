afb6c3a22857c4bfb86a5c01829948605ff7b7b2
kes3035 <kes3035@gmail.com>
2023-08-17 19:06:00 +0900

    Fix: 어노테이션 뷰 주석처리

--- DRBS-HomeComing/View/VC/MapVC/AnnotationView.swift
+++ DRBS-HomeComing/View/VC/MapVC/AnnotationView.swift
@@ -1,23 +1,23 @@
 import UIKit
 import MapKit
-import Then
-import SnapKit
+//import Then
+//import SnapKit
 
 class AnnotationView: MKAnnotationView {
     //MARK: - Properties
     
-    private let view = UIImageView().then {
-        $0.backgroundColor = .clear
-        $0.clipsToBounds = true
-        $0.image = UIImage(systemName: "house")
-        $0.contentMode = .scaleAspectFit
-        $0.tintColor = .white
-    }
-    
-    private let backgroundView = UIView().then {
-        $0.backgroundColor = Constant.appColor
-        $0.clipsToBounds = true
-    }
+//    private let view = UIImageView().then {
+//        $0.backgroundColor = .clear
+//        $0.clipsToBounds = true
+//        $0.image = UIImage(systemName: "house")
+//        $0.contentMode = .scaleAspectFit
+//        $0.tintColor = .white
+//    }
+//
+//    private let backgroundView = UIView().then {
+//        $0.backgroundColor = Constant.appColor
+//        $0.clipsToBounds = true
+//    }
     
     
     //MARK: - LifeCycle
@@ -34,21 +34,21 @@ class AnnotationView: MKAnnotationView {
     
     //MARK: - Helpers
 
-    private func configureUI() {
-        backgroundColor = .clear
-        backgroundView.addSubview(view)
-        self.addSubviews(backgroundView)
-        
-        backgroundView.snp.makeConstraints {
-            $0.width.height.equalTo(30)
-            $0.centerX.equalTo(self.snp.centerX)
-            $0.centerY.equalTo(self.snp.centerY)}
-        
-        view.snp.makeConstraints {
-            $0.width.height.equalTo(25)
-            $0.centerX.equalToSuperview()
-            $0.centerY.equalToSuperview()}
-        backgroundView.layer.cornerRadius = 15
-    }
+//    private func configureUI() {
+//        backgroundColor = .clear
+//        backgroundView.addSubview(view)
+//        self.addSubviews(backgroundView)
+//
+//        backgroundView.snp.makeConstraints {
+//            $0.width.height.equalTo(30)
+//            $0.centerX.equalTo(self.snp.centerX)
+//            $0.centerY.equalTo(self.snp.centerY)}
+//
+//        view.snp.makeConstraints {
+//            $0.width.height.equalTo(25)
+//            $0.centerX.equalToSuperview()
+//            $0.centerY.equalToSuperview()}
+//        backgroundView.layer.cornerRadius = 15
+//    }
     
 }
