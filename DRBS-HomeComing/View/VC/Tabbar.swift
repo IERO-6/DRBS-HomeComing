8a3b2e6e0e2295efb9e472fada364994e1b1118e
kes3035 <kes3035@gmail.com>
2023-09-28 13:47:27 +0900

    Fix: Tabbar 스크롤 다운 시 구분선 사라지는 오류 수정

--- DRBS-HomeComing/View/VC/Tabbar.swift
+++ DRBS-HomeComing/View/VC/Tabbar.swift
@@ -9,8 +9,13 @@ final class Tabbar: UITabBarController {
     }
     //MARK: - Helpers
     private func configureTabbar() {
+        let appearance = UITabBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        UITabBar.appearance().standardAppearance = appearance
+        UITabBar.appearance().scrollEdgeAppearance = UITabBar.appearance().standardAppearance
+        self.tabBar.standardAppearance = appearance
         self.tabBar.isTranslucent = false
-        self.tabBar.backgroundColor = .white
+        self.tabBar.barTintColor = .white
         self.tabBar.tintColor = Constant.appColor
     }
 }
